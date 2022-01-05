8d79df7230702ff37150efb7fcf7221c6b587ca1
Examine88 <Naufalazmi020@gmail.com>
2022-01-05 09:39:06 +0800

    added validation in form diagnosa

--- app/src/main/java/com/example/ewss/ui/main/diagnosa/DiagnosaFragment.kt
+++ app/src/main/java/com/example/ewss/ui/main/diagnosa/DiagnosaFragment.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import com.example.core.data.Resource
 import com.example.core.presentation.model.DiagnosaForm
 import com.example.ewss.databinding.FragmentDiagnosaBinding
 import com.example.ewss.ui.main.diagnosa.result.DiagnosaResultFragment
 import com.example.ewss.utils.StatusPatient.setKesadaranAdapter
+import com.google.android.material.textfield.TextInputLayout
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class DiagnosaFragment : Fragment() {
@@ -30,8 +33,8 @@ class DiagnosaFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
-        setupListener()
         setupAdapter()
+        setupListener()
         super.onViewCreated(view, savedInstanceState)
     }
 
@@ -39,39 +42,132 @@ class DiagnosaFragment : Fragment() {
         requireContext().setKesadaranAdapter(binding.kesadaranAutocomplete)
     }
 
+
     private fun setupListener() {
-        binding.btnDiagnosa.setOnClickListener {
-            diagnosaViewModel.diagnosa(getDiagnosaForm()).observe(requireActivity(), {
-                when (it) {
-                    is Resource.Error -> {
-                        binding.loading.visibility = View.GONE
-                        Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
-                    }
-                    is Resource.Loading -> binding.loading.visibility = View.VISIBLE
-                    is Resource.Success -> {
-                        binding.loading.visibility = View.GONE
-                        val resultDialog = DiagnosaResultFragment()
-                        resultDialog.arguments = Bundle().apply {
-                            putString(DiagnosaResultFragment.DATA_RESULT, it.data?.result)
-                            putString(DiagnosaResultFragment.DATA_MESSAGE, it.data?.detailResult)
+        with(binding) {
+            inputFullname.setAutoClearError(fullname)
+            inputAddress.setAutoClearError(address)
+            inputAge.setAutoClearError(age)
+            inputNadi.setAutoClearError(nadi)
+            inputPernafasan.setAutoClearError(pernafasan)
+            inputSuhu.setAutoClearError(suhu)
+            inputTekananDarah.setAutoClearError(tekananDarah)
+            kesadaranAutocomplete.setAutoClearError(kesadaran)
+
+
+            btnDiagnosa.setOnClickListener {
+                if (isValidForm()) {
+                    diagnosaViewModel.diagnosa(getDiagnosaForm()).observe(requireActivity(), {
+                        when (it) {
+                            is Resource.Error -> {
+                                binding.loading.visibility = View.GONE
+                                Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                            is Resource.Loading -> binding.loading.visibility = View.VISIBLE
+                            is Resource.Success -> {
+                                binding.loading.visibility = View.GONE
+                                val resultDialog = DiagnosaResultFragment()
+                                resultDialog.arguments = Bundle().apply {
+                                    putString(DiagnosaResultFragment.DATA_RESULT, it.data?.result)
+                                    putString(
+                                        DiagnosaResultFragment.DATA_MESSAGE,
+                                        it.data?.detailResult
+                                    )
+                                }
+                                resultDialog.show(
+                                    childFragmentManager,
+                                    DiagnosaResultFragment::javaClass.name
+                                )
+                                clearForm()
+                            }
                         }
-                        resultDialog.show(
-                            childFragmentManager,
-                            DiagnosaResultFragment::javaClass.name
-                        )
-                        clearForm()
-                    }
+                    })
+                } else {
+                    showErrorMessage()
                 }
-            })
+            }
         }
     }
 
-    private fun clearForm(){
-        with(binding){
+    private fun isValidForm(): Boolean =
+        binding.inputFullname.text.toString().isNotEmpty() &&
+                binding.inputAddress.text.toString().isNotEmpty() &&
+                binding.inputAge.text.toString().isNotEmpty() &&
+                binding.inputNadi.text.toString().isNotEmpty() &&
+                binding.inputPernafasan.text.toString().isNotEmpty() &&
+                binding.inputSuhu.text.toString().isNotEmpty() &&
+                binding.inputTekananDarah.text.toString().isNotEmpty() &&
+                binding.kesadaranAutocomplete.text.toString().isNotEmpty()
+
+    private fun formErrorHandler(
+        viewInput: TextInputLayout,
+        errorCondition: Boolean,
+        errorMessage: String
+    ) {
+        if (errorCondition) viewInput.error = errorMessage
+    }
+
+    private fun showErrorMessage() {
+        with(binding) {
+            formErrorHandler(
+                fullname,
+                inputFullname.text.toString().isEmpty(),
+                "Nama tidak boleh kosong"
+            )
+            formErrorHandler(
+                address,
+                inputAddress.text.toString().isEmpty(),
+                "Alamat tidak boleh kosong"
+            )
+            formErrorHandler(
+                age,
+                inputAge.text.toString().isEmpty(),
+                "Umur tidak boleh kosong"
+            )
+            formErrorHandler(
+                nadi,
+                inputNadi.text.toString().isEmpty(),
+                "Denyut nadi tidak boleh kosong"
+            )
+            formErrorHandler(
+                pernafasan,
+                inputPernafasan.text.toString().isEmpty(),
+                "Pernafasan tidak boleh kosong"
+            )
+            formErrorHandler(
+                suhu,
+                inputSuhu.text.toString().isEmpty(),
+                "Suhu tidak boleh kosong"
+            )
+            formErrorHandler(
+                tekananDarah,
+                inputTekananDarah.text.toString().isEmpty(),
+                "Tekanan darah tidak boleh kosong"
+            )
+            formErrorHandler(
+                kesadaran,
+                kesadaranAutocomplete.text.toString().isEmpty(),
+                "Kesadaran tidak boleh kosong"
+            )
+
+        }
+    }
+
+
+    private fun EditText.setAutoClearError(viewInput: TextInputLayout) {
+        addTextChangedListener {
+            if (it.toString().isNotEmpty() && viewInput.error != null) {
+                viewInput.error = null
+            }
+        }
+    }
+
+    private fun clearForm() {
+        with(binding) {
             inputFullname.setText("")
             inputAge.setText("")
             inputAddress.setText("")
-            kesadaranAutocomplete.setText("")
             inputSuhu.setText("")
             inputTekananDarah.setText("")
             inputPernafasan.setText("")
@@ -85,11 +181,11 @@ class DiagnosaFragment : Fragment() {
                 fullname = inputFullname.text.toString(),
                 address = inputAddress.text.toString(),
                 age = inputAge.text.toString().toInt(),
-                kesadaran = kesadaranAutocomplete.text.toString(),
                 denyutNadi = inputNadi.text.toString().toInt(),
                 suhu = inputSuhu.text.toString().toDouble(),
                 pernafasan = inputPernafasan.text.toString().toInt(),
-                tekananDarah = inputTekananDarah.text.toString().toInt()
+                tekananDarah = inputTekananDarah.text.toString().toInt(),
+                kesadaran = kesadaranAutocomplete.text.toString()
             )
         }
     }
